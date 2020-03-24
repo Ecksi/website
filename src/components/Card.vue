65845a56d144d5db7191a3d010f8c305887be9bf
ecksi <jimecksi@gmail.com>
2020-03-23 21:35:16 -0600

    Add initial styles

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
-    <img src="../assets/img/yo.png" alt="Thumbs up from your homie">
-    <div>
-      <h2>Jim Dechant</h2>
-      <div>Software Developer</div>
-      <div>jimecksi@gmail.com</div>
-      <div>https://github.com/Ecksi</div>
-      <div>https://www.linkedin.com/in/jimecksi/</div>
+  <div class="md:flex bg-white rounded-lg p-6">
+    <img class="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6"  src="../assets/img/yo.png" alt="Thumbs up from your homie">
+    <div class="text-center md:text-left">
+      <h2 class="text-lg">Jim Dechant</h2>
+      <div class="text-gray-600">Software Developer</div>
+      <div class="text-gray-600">jimecksi@gmail.com</div>
+      <div class="text-gray-600">https://github.com/Ecksi</div>
+      <div class="text-gray-600">https://www.linkedin.com/in/jimecksi/</div>
     </div>
   </div>  
 </template>
