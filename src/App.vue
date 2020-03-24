ade8ba59ded963b20c4dcd1ee285a1e749a7b6ea
Jim Ecksi <jimecksi@gmail.com>
2020-03-23 20:28:49 -0600

    Remove cli boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    Jim Dechant
+    Software Developer
+    https://www.linkedin.com/in/jimecksi/
+    https://github.com/Ecksi
+    Coming soon - https://www.jimecksi.com
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App'
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
